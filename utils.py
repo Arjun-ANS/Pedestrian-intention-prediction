00ecdbbf4f0b18acbbbbd261e43d6a29b9053bfd
Arjun-ANS <143984587+Arjun-ANS@users.noreply.github.com>
2024-10-26 15:22:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -58,7 +58,7 @@ def get_pose(img_sequences,
     set_poses = {}
     for s in set_poses_list:
         print(s)
-        with open(os.path.join('/kaggle/input/pedest28/data/features/jaad/poses','pose_set01.pkl'),'rb') as fid:
+        with open(os.path.join('/kaggle/input/pedest29/data/features/jaad/poses','pose_set01.pkl'),'rb') as fid:
             try:
                 p = pickle.load(fid)
             except:
